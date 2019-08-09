92bf93e719ccebfbb5bf2e6249a98f7f4769e80e
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-09 12:19:51 +0100

    initial commit

---
+++ app/src/main/java/com/oganbelema/navcontrollerdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.oganbelema.navcontrollerdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
