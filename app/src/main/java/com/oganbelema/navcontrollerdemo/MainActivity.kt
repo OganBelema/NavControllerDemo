fb314ca4c11b5b91154a68ed498007e37debf9bf
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-09 13:49:15 +0100

    Add navigation action

--- app/src/main/java/com/oganbelema/navcontrollerdemo/MainActivity.kt
+++ app/src/main/java/com/oganbelema/navcontrollerdemo/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.oganbelema.navcontrollerdemo
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SecondFragment.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
